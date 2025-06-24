8e891f12c3782cf1e3f661941731c2ccfa76d101
abduthebeast <abdullaharsh01@gmail.com>
2025-06-24 23:05:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function formatNum(n) {
   if (n < 1e6) return (n / 1e3).toFixed(2) + "K";
   if (n < 1e9) return (n / 1e6).toFixed(2) + "M";
   if (n < 1e12) return (n / 1e9).toFixed(2) + "B";
+  if (n < 1e15) return (n / 1e12).toFixed(2) + "T";
   return n.toExponential(3).replace('+', '').toUpperCase();
 }
 
